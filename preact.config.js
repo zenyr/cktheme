edee6d2e1b932b694a48dbdd3b8e9b1d82e09ece
Jinhyeok <jhlee1@c-won.com>
2017-08-09 18:42:23 +0900

    fix async generator runtime error

--- preact.config.js
+++ preact.config.js
@@ -4,6 +4,7 @@ export default (config, env, helpers) => {
     const babelConfig = rule.options;
 
     babelConfig.plugins.push('transform-flow-strip-types');
+    babelConfig.plugins.push('transform-regenerator');
   } else {
     throw new Error('Failed to modify babel-loader');
   }
